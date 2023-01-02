fefafa9df176209c12f0010e005a78e0bed882d4
Meda Ruthu <51402756+Ruthu417@users.noreply.github.com>
2023-01-02 15:49:56 +0530

    Add files via upload

---
+++ DA workshop/second.py
@@ -0,0 +1,15 @@
+st=input()
+#I want to replace the first and last chars with * in the given str
+#st=pallavi:*allav*
+n=len(st)
+for ix in range(n):
+    if ix==0 or ix==n-1:
+        print('*',end="")
+    else:
+        print(st[ix],end="")
+print("data added using append")
+print("data added using append")
+print("new to progamming")
+print("new to progamming")
+print("this is at the end
+")hi welcome to DA Workshophi welcome to DA Workshop
